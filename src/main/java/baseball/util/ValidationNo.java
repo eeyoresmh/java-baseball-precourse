88949a6e4a0e4402e14b3041f67ef9b267147104
eeyoresmh <eeyoresmin@gmail.com>
2022-04-14 15:51:44 +0900

    feat: ValidationNo 기능
    사용자 입력값에 대한 validation 기능 및 test 모듈
    해당 기능을 통해 추후 Ball에서의 validation은 불필요하다.
    음수를 하면 자릿수가 초과하고,
    자릿수를 맞추면 숫자로 변환이 에러가 발생하기때문

---
+++ src/main/java/baseball/util/ValidationNo.java
@@ -0,0 +1,43 @@
+package baseball.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ValidationNo {
+    private final static String SIZE_EXCEPTION_MESSAGE = "3자리 숫자로 입력하십시오.";
+    private final static String NO_EXCEPTION_MESSAGE = "중복 값이 존재합니다.";
+    private final static int SIZE = 3;
+
+    public boolean isOk(String numbers) {
+        checkSize(numbers);
+        checkDuplicateNumber(numbers);
+
+        return true;
+    }
+
+    private void checkSize(String numbers) {
+        if (numbers.length() != SIZE) {
+            throw new IllegalArgumentException(SIZE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void checkDuplicateNumber(String numbers) {
+        Set<Integer> checkedNum = new HashSet<>();
+
+        for (int i = 0; i < numbers.length(); i++) {
+            checkedNum.add(checkNumeric(numbers.charAt(i)));
+        }
+
+        if (checkedNum.size() != SIZE) {
+            throw new IllegalArgumentException(NO_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private int checkNumeric(char targetChar) {
+        if (!Character.isDigit(targetChar)) {
+            System.out.println(targetChar + " : " + Character.isDigit(targetChar));
+            throw new IllegalArgumentException(SIZE_EXCEPTION_MESSAGE);
+        }
+        return Character.getNumericValue(targetChar);
+    }
+}
---
+++ src/test/java/baseball/util/ValidationNoTest.java
@@ -0,0 +1,60 @@
+package baseball.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class ValidationNoTest {
+
+    ValidationNo validationNo = new ValidationNo();
+
+    @Test
+    public void 자릿수_체크() {
+        String numbers = "123";
+
+        assertThat(validationNo.isOk(numbers)).isEqualTo(true);
+    }
+
+    @Test
+    public void 자릿수_fail_체크() {
+        String numbers = "12";
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> validationNo.isOk(numbers));
+    }
+
+    @Test
+    public void 중복숫자_성공_체크() {
+        String numbers = "123";
+        validationNo.isOk(numbers);
+    }
+
+    @Test
+    public void 중복숫자_fail_체크() {
+        String numbers = "121";
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> validationNo.isOk(numbers));
+    }
+
+    @Test
+    public void 문자포함_fail_체크() {
+        String numbers = "일23";
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> validationNo.isOk(numbers));
+    }
+
+    @Test
+    public void 음수포함_fail_체크() {
+        String numbers = "-123";
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> validationNo.isOk(numbers));
+    }
+
+    @Test
+    public void 음수포함_2자리_fail_체크() {
+        String numbers = "-23";
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> validationNo.isOk(numbers));
+    }
+}
