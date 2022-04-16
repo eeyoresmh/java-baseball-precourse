a70cd4887e414cdfed4a9d417131f06d99b99955
eeyoresmh <eeyoresmin@gmail.com>
2022-04-17 02:02:21 +0900

    feat: 결과 문구 출력 추가

--- src/main/java/baseball/domain/Result.java
+++ src/main/java/baseball/domain/Result.java
@@ -3,6 +3,11 @@ package baseball.domain;
 import baseball.BallStatus;
 
 public class Result {
+	private static final String NOTHING = "낫싱";
+	private static final String STRIKE_FORMAT = "%d스트라이크";
+	private static final String BALL_FORMAT = "%d볼";
+	private static final String BALL_AND_STRIKE_FORMAT = "%d볼 %d스트라이크";
+
 	private int strike = 0;
 	private int ball = 0;
 
@@ -31,4 +36,30 @@ public class Result {
 	public boolean isAllStrike() {
 		return strike == 3;
 	}
+
+	public boolean isStrike() {
+		return strike > 0;
+	}
+
+	public boolean isBall() {
+		return ball > 0;
+	}
+
+	@Override
+	public String toString() {
+		if (isStrike() && isBall()) {
+			return String.format(BALL_AND_STRIKE_FORMAT, ball, strike);
+		}
+
+		if (isBall()) {
+			return String.format(BALL_FORMAT, ball);
+		}
+
+		if (isStrike()) {
+			return String.format(STRIKE_FORMAT, strike);
+		}
+
+		return NOTHING;
+	}
+
 }
